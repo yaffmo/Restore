9409125511bcc4acfe0d5e4f7dae6132584b7c38
yaffmo <yaffmo@gmail.com>
2022-04-17 15:10:11 +0800

    card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Product } from "./type";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 function App() {
   const [products, setProduct] = useState<Product[]>([]);
@@ -16,9 +21,24 @@ function App() {
     <div className="App">
       <ul>
         {products.map((item, index) => (
-          <li key={index}>
-            {item.id}-{item.name}
-          </li>
+          <ul key={index}>
+            <Card sx={{ maxWidth: 345, m: 5 }}>
+              <CardMedia
+                component="img"
+                height="200"
+                image={item.pictureUrl}
+                alt={item.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {item.description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </ul>
         ))}
       </ul>
     </div>
